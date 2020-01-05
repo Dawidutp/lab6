07c6cfaa77349356ec3c659e78286e7d078e20db
Maciuee <59364005+Maciuee@users.noreply.github.com>
2020-01-05 11:15:58 +0100

    Name change

--- silniaf.c
+++ silniaf.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
 #include <stdlib.h>
 int silnia(int x){
-int n, liczba=1;
+int n, num=1;
     for(n=1;n<=x;n++){
-        liczba=liczba*n;
-       if (n==x) printf("%d\n", liczba);
+        num=num*n;
+       if (n==x) printf("%d\n", num);
     }
-return liczba;
+return num;
 }
