9b4cfdda85fb12039b12283f6fadd89446711862
Dawidutp <59363102+Dawidutp@users.noreply.github.com>
2020-01-05 12:55:24 +0100

    Add files via upload

---
+++ dzielenie.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+void Dzielenie()
+{
+    int a,b,c;
+    printf("Podaj liczby ,ktore chcesz podzielic:\n");
+    scanf("%d%d",&a,&b);
+    c=a/b;
+    printf("Iloraz tych liczb to: %d",c);
+}
