9b1d814fbc61b3dfafed8810bc563b85d688cef0
Sticz <59374639+Sticz10@users.noreply.github.com>
2020-01-05 13:03:17 +0100

    Add files via upload

--- pierwiastek.c
+++ pierwiastek.c
@@ -2,15 +2,11 @@
 #include <stdlib.h>
 #include <math.h>
 
-int Pierwiastek()
+void Pierwiastek()
 {
-
-    float x;
-
+    float x
     printf("Podaj liczbe: ");
     scanf("%f", &x);
 
     printf("sqrt(%f) = %f", x, sqrt(x));
-
-    return 0;
 }
