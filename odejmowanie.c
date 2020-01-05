96b18b58c26b07cdeca8746b5940b8ac98ffd1a5
Dawidutp <59363102+Dawidutp@users.noreply.github.com>
2020-01-05 13:49:04 +0100

    Add files via upload

---
+++ odejmowanie.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+void odejmowanie()
+{
+    int a,b,c;
+    printf("Podaj liczby ,ktore chcesz odjac:\n");
+    scanf("%d%d",&a,&b);
+    c=a-b;
+    printf("Roznica tych liczb to: %d",c);
+}
